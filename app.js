f0ef8733756f91a6d8fbb7873e4f767507d78851
Med-azaza <hamaaz666@gmail.com>
2021-03-26 00:36:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function (){
+    const rotation = ()=>{
+        let d= new Date();
+        let s=d.getSeconds();
+        let h=d.getHours();
+        let m=d.getMinutes();
+        s*=6;
+        h*=30;
+        h+=m/2;
+        $('#sec-stick').css({transform:'rotate('+s.toString()+'deg)'});
+        $('#min-stick').css({transform:'rotate('+h.toString()+'deg)'});
+    }
+    let t=setInterval(rotation,1000);
+})
